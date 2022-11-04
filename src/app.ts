4d38d2e7dece4ef93fb1e37b9138d299f47caf2b
Onkeljoe <dev@onkeljoe.de>
2022-11-04 17:13:22 +0100

    add some testing-routes

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,27 @@ import express, { Request, Response } from "express";
 const port = 3030;
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req: Request, res: Response) => {
   return res.send("Hello World");
 });
 
 app.get("/health/", (req: Request, res: Response) => {
   return res.send("alive");
+});
+
+app.get("/bribefile/", (req: Request, res: Response) => {
+  res.json({
+    round: 99,
+    status: "pending",
+  });
+});
+
+app.post("/api/data", (req: Request, res: Response) => {
+  console.log(req.body);
+
+  return res.sendStatus(200);
 });
 
 app.listen(port, () => {

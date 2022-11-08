600c80d9b9e0bfff1e3e7f96f15834903b0a3af9
Onkeljoe <dev@onkeljoe.de>
2022-11-08 16:50:17 +0100

    Prepare structure for 3 reading Endpoints

--- src/routes.ts
+++ src/routes.ts
@@ -1,25 +1,31 @@
 import { Express, Request, Response, NextFunction } from "express";
 
 function routes(app: Express) {
-  app.get("/", (req: Request, res: Response) => {
-    return res.send("Nothing to see here");
-  });
-
-  app.get("/health/", (req: Request, res: Response) => {
+  app.get("/api/v1/health", (req: Request, res: Response) => {
     return res.send("alive");
   });
 
-  app.get("/bribefile/", (req: Request, res: Response) => {
+  app.get("/api/v1/rounds", (req: Request, res: Response) => {
+    console.log(req.body);
+
+    return res.sendStatus(200);
+  });
+
+  app.get("/api/v1/bribedata/round", (req: Request, res: Response) => {
     res.json({
       round: 99,
       status: "pending",
     });
   });
 
-  app.post("/api/data", (req: Request, res: Response) => {
+  app.get("/api/v1/chartdata", (req: Request, res: Response) => {
     console.log(req.body);
 
     return res.sendStatus(200);
+  });
+
+  app.all("/", (req: Request, res: Response) => {
+    return res.send("Nothing to see here");
   });
 }
 

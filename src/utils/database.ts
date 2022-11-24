9ed6f069277269b69676cdb0ca9b55f499435602
Johannes Heinemann <defi@onkeljoe.de>
2022-11-24 16:09:50 +0100

    cleanup

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -58,3 +58,4 @@ export function disconnectFromDb() {
 // // Export a module-scoped MongoClient promise. By doing this in a
 // // separate module, the client can be shared across functions.
 // export default clientPromise;
+ 

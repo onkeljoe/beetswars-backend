34b17e032744440b09f2bca10ca771addaf749ba
Johannes Heinemann <defi@onkeljoe.de>
2022-12-05 17:07:22 +0100

    provide API doc

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import express, { Request, Response } from "express";
+import swaggerUi from "swagger-ui-express";
+import yaml from "yamljs";
 import logger from "./utils/logger";
 import { config } from "./utils/config";
 import { db } from "./utils/database";
@@ -9,6 +11,15 @@ import cors from "cors";
 import api from "./api";
 
 const app = express();
+var swaggerdoc;
+try {
+  swaggerdoc = yaml.load("dist/beetswars-API.yml");
+  logger.info("dist");
+} catch (error) {
+  swaggerdoc = yaml.load("src/beetswars-API.yml");
+  logger.info("src");
+}
+
 try {
   const myDb = db;
   logger.info(`DB: ${typeof myDb}`);
@@ -20,6 +31,7 @@ app.use(helmet()); // add security layer
 app.use(cors()); // allow cross-origin requests
 app.use(express.json()); // allow JSON in POST
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerdoc));
 app.get("/", (req: Request, res: Response<string>) => {
   return res.send("Nothing to see here.");
 });

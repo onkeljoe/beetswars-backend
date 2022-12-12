0f9fe80d090199b8ee5a7aa5ff78828185b630a2
Johannes Heinemann <defi@onkeljoe.de>
2022-12-12 19:17:47 +0100

    add latest endpoints

--- src/api/bribedata/bribedata.routes.ts
+++ src/api/bribedata/bribedata.routes.ts
@@ -5,8 +5,10 @@ const router = express.Router();
 
 router.get("/", BribedataHandlers.getList);
 router.get("/list", BribedataHandlers.getList);
+router.get("/latest", BribedataHandlers.findLatest);
 router.get("/:round", BribedataHandlers.findOne);
 router.post("/:round", BribedataHandlers.insertRound);
+router.post("/latest/:round", BribedataHandlers.setLatest);
 router.post("/:round/:voteindex", BribedataHandlers.insertBribe);
 router.post("/token/:round/:key", BribedataHandlers.insertToken);
 router.delete("/:round", BribedataHandlers.deleteRound);

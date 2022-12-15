6ecf7493f2094a8b1c895a88f7f0c776def20fff
Johannes Heinemann <defi@onkeljoe.de>
2022-12-15 19:52:32 +0100

    add to voteend cron endpoint

--- src/api/cron/cron.helper.ts
+++ src/api/cron/cron.helper.ts
@@ -1,46 +1,19 @@
 import { readOne } from "../../utils/database";
 import { Bribefile } from "../bribedata/bribedata.model";
 import { Chartdata } from "../chartdata/chartdata.model";
-// import { request, gql } from "graphql-request";
 import { getSnapshotProposal, getSnapshotVotes } from "../../utils/snapshot";
-
-// async function getSnapshotProposal(id: string) {
-//   const QUERY = gql`
-//     query Proposal {
-//       proposal(id:"${id}") {
-//         start
-//         end
-//       }
-//     }
-//   `;
-//   const data = await request("https://hub.snapshot.org/graphql", QUERY);
-//   return data.proposal as { start: number; end: number };
-// }
-
-// async function getSnapshotVotes(id: string) {
-//   const QUERY = gql`
-//     query Votes {
-//       votes(
-//         first: 1000
-//         where: {
-//           proposal: "${id}"
-//         }
-//       ) {
-//         choice
-//         vp
-//       }
-//     }
-//   `;
-//   const data = await request("https://hub.snapshot.org/graphql", QUERY);
-//   return data.votes as { choice: { key: number }; vp: number }[];
-//   // return data.proposal as { start: number; end: number };
-// }
+import { map } from "zod";
+import { getBeetsPrice } from "../../utils/coingecko";
 
 export async function getData(round: string) {
   const newData = {} as Chartdata;
   const bribefile = await readOne<Bribefile>("bribedata", round);
   if (!bribefile) return newData; //empty
+  // console.log(bribefile.)
   const proposal = bribefile.snapshot;
+  const bribedOffers = bribefile.bribedata.map((x) =>
+    (x.voteindex + 1).toString()
+  );
   const roundProposal = await getSnapshotProposal(proposal);
   const end = roundProposal.end;
   const votes = await getSnapshotVotes(proposal);
@@ -48,23 +21,36 @@ export async function getData(round: string) {
   const totalVotes =
     totalVoter === 0
       ? 0
-      : votes
+      : +votes
           .map((x) => x.vp)
           .reduce((sum, x) => sum + x)
           .toFixed();
 
+  // calculate bribed votes
+  var bribedVotes = 0;
+  votes.forEach(
+    (element: { choice: { [key: string]: number }; vp: number }) => {
+      const total = Object.values(element.choice).reduce((a, b) => a + b);
+      bribedOffers.map((x) => {
+        if (element.choice[x]) {
+          bribedVotes += (element.vp * element.choice[x]) / total;
+        }
+      });
+    }
+  );
+
+  const priceBeets = await getBeetsPrice(end);
+
   // fill data
   newData.round = round;
   newData.totalVoter = totalVoter;
-  newData.totalVotes = +totalVotes;
+  newData.totalVotes = totalVotes;
   newData.totalBriber = bribefile.bribedata.length;
   newData.totalBribes = 0; //TODO
-  newData.bribedVotes = 0; //TODO
+  newData.bribedVotes = +bribedVotes.toFixed();
   newData.voteEnd = end;
-  newData.priceBeets = 0; //TODO
-  newData.priceFbeets = 0;
-
-  //   console.log("total: ", newData.totalBriber);
+  newData.priceBeets = priceBeets;
+  newData.priceFbeets = 0; //TODO
 
   return newData;
 }

5dd39159c169ae013cbe37a7649c0ec20e9d03ed
Johannes Heinemann <defi@onkeljoe.de>
2022-12-20 20:14:01 +0100

    finish chartdata voteend

--- src/api/cron/cron.handler.ts
+++ src/api/cron/cron.handler.ts
@@ -1,5 +1,4 @@
 import { Request, Response } from "express";
-// import { z, ZodError } from "zod";
 
 import { db, readOne } from "../../utils/database";
 import logger from "../../utils/logger";
@@ -13,7 +12,10 @@ export async function insertVoteEnd(req: Request, res: Response) {
   const round = await readOne<roundtype>("latest", "latestkey");
   if (!round) return;
   const newRound = await getData(round.key);
-  console.log("newRound: ", newRound);
   logger.info("Vote End recorded");
-  return res.send("OK");
+  const checkChartdataEntry = await readOne<Chartdata>("chartdata", round.key);
+  if (checkChartdataEntry) return res.status(409).send("duplicate entry");
+  const result = await chartTable.set(round.key, newRound);
+  if (!result) res.status(500).send("Error inserting Chartdata");
+  return res.status(201).json(result.props);
 }
